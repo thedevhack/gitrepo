6ebf7d628dff5e9c63985ad66a267537f96633dc
Ansu-Pc <ansu-pc@Ansu-Pcs-Mac.local>
2019-07-03 14:33:04 +0530

    Intial Project Setup

--- FirstApp/Controller/ViewController.swift
+++ FirstApp/Controller/ViewController.swift
@@ -13,8 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        let storyboard = UIStoryboard(name: "LoginStoryBoard", bundle: nil)
+        let controller = storyboard.instantiateViewController(withIdentifier: "loginVC")
+        present(controller, animated: true, completion: nil)    }
 
 }
 
---
+++ FirstApp/Utilities/Extensons.swift
@@ -0,0 +1,9 @@
+//
+//  Extensons.swift
+//  FirstApp
+//
+//  Created by Ansu-Pc on 03/07/19.
+//  Copyright © 2019 Ansu-Pc. All rights reserved.
+//
+
+import Foundation
