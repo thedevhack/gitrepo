ccad11e3c165b676dcc07653e6a77a76226fedf4
Ansu-Pc <ansu-pc@Ansu-Pcs-Mac.local>
2019-07-09 23:49:43 +0530

    Coded Till Image Stored in DAtabase with book details Retrieval Remaining

--- FirstApp/Controller/AddBookVC.swift
+++ FirstApp/Controller/AddBookVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import Firebase
 
-class AddBookVC: UIViewController {
+class AddBookVC: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate{
     var refAddBooks: DatabaseReference!
     @IBOutlet weak var booktitleTxt: UITextField!
     @IBOutlet weak var bookauthorTxt: UITextField!
@@ -23,23 +23,75 @@ class AddBookVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         refAddBooks = Database.database().reference().child("BooksDetails")
+        bookImageChoose.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(HandleSelectProfileImageView)))
+        bookImageChoose.isUserInteractionEnabled = true
+        
+        }
+    //code for launching image picker
+    @objc func HandleSelectProfileImageView() {
+    let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+}
+    //code for selecting image
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        var selectedImage: UIImage?
+        if let editedImage = info[.editedImage] as? UIImage {
+            selectedImage = editedImage
+            self.bookImageChoose.image = selectedImage
+            picker.dismiss(animated: true, completion: nil)
+        } else if let originalImage = info[.originalImage] as? UIImage {
+            selectedImage = originalImage
+            self.bookImageChoose.image = selectedImage
+            picker.dismiss(animated: true, completion: nil)
+        }
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("Canceled Picker")
+        dismiss(animated: true, completion: nil)
     }
     func addBooksDetails() {
         let key = refAddBooks.childByAutoId().key
-        
-        let BooksDetails = ["id":key,
-                            "BooksTitle":booktitleTxt.text! as String ,
-                            "BooksAuthor":bookauthorTxt.text! as String ,
-                            "BooksCondition":bookconditionTxt.text! as String ,
-                            "BookOriginalPrice":bookoriginalpriceTxt.text! as String ,
-                            "BookOfferPrice":bookofferpriceTxt.text! as String ,
-                            "BookCategory":bookcategoryTxt.text! as String
-                             ]
-        refAddBooks.child(key!).setValue(BooksDetails)
+        let bookimagename = NSUUID().uuidString
+        let addImage = Storage.storage().reference().child("BookImagesPics").child("\(bookimagename).png")
+        if let uploadData = bookImageChoose.image?.pngData() {
+            addImage.putData(uploadData, metadata: nil) { (metadata, error) in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                addImage.downloadURL { (url, error) in
+                    if error != nil{
+                        print(error!)
+                    }
+                guard let downloadURL = url else {return}
+                let downloadURLString = downloadURL.absoluteString
+                let BooksDetails = ["id":key,
+                                    "BooksTitle":self.booktitleTxt.text! as String ,
+                                    "BooksAuthor":self.bookauthorTxt.text! as String ,
+                                    "BooksCondition":self.bookconditionTxt.text! as String ,
+                                    "BookOriginalPrice":self.bookoriginalpriceTxt.text! as String ,
+                                    "BookOfferPrice":self.bookofferpriceTxt.text! as String ,
+                                    "BookCategory":self.bookcategoryTxt.text! as String ,
+                                    "BookImageUrl":downloadURLString
+                    ]
+                    self.AddBookIntoDatabase(BooksDetails: BooksDetails as [String : AnyObject])
+    
+                }
+            }
+        }
     }
-    @IBAction func bookdetailsubmitClicked(_ sender: Any)
-    {
+    func AddBookIntoDatabase(BooksDetails:[String:AnyObject]) {
+    let key = refAddBooks.childByAutoId().key
+    refAddBooks = Database.database().reference().child("BooksDetails")
+        refAddBooks.child(key!).setValue(BooksDetails)
+}
+    
+    @IBAction func submitBooksDetails(_ sender: UIButton) {
         addBooksDetails()
     }
-    
 }
